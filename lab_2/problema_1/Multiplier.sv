08ec66a3042dfc972dde289c5f072635337c6915
joscCE <j.campos@estudiantec.cr>
2025-03-19 23:12:58 -0600

    parametri

--- lab_2/problema_1/Multiplier.sv
+++ lab_2/problema_1/Multiplier.sv
@@ -1,58 +1,43 @@
-module Multiplier (
-    input logic [3:0] a, b,
-    output logic [7:0] p
+module Multiplier #(parameter N = 4) (
+    input logic [N-1:0] a, b,
+    output logic [(2*N)-1:0] p
 );
 
-    
-    logic [3:0] partials [0:3];
-
-    
+    logic [N-1:0] partials [0:N-1];
     genvar i, j;
+    
     generate
-        for (i = 0; i < 4; i++) begin : partial_product_gen
-            for (j = 0; j < 4; j++) begin : and_gen
+        for (i = 0; i < N; i++) begin : partial_product_gen
+            for (j = 0; j < N; j++) begin : and_gen
                 assign partials[i][j] = a[i] & b[j]; 
             end
         end
     endgenerate
 
+    logic [(2*N)-1:0] shifted_partial [0:N-1];
     
-    logic [7:0] shifted_partial [0:3];
-
     generate
-        for (i = 0; i < 4; i++) begin : shift_gen
+        for (i = 0; i < N; i++) begin : shift_gen
             assign shifted_partial[i] = {partials[i], {i{1'b0}}}; 
         end
     endgenerate
-
-
-    logic [7:0] sum1, sum2, final_product;
-
-
-    FullAdder FA0 (
-        .a(shifted_partial[0]), 
-        .b(shifted_partial[1]), 
-        .cin(0),
-        .s(sum1), 
-        .cout()
-    );
-
-    FullAdder FA1 (
-        .a(sum1), 
-        .b(shifted_partial[2]), 
-        .cin(0),
-        .s(sum2), 
-        .cout()
-    );
-
-    FullAdder FA2 (
-        .a(sum2), 
-        .b(shifted_partial[3]), 
-        .cin(0),
-        .s(final_product), 
-        .cout()
-    );
-
-    assign p = final_product; 
-
+    
+    logic [(2*N)-1:0] sum[N-1:0];
+    
+    assign sum[0] = shifted_partial[0];
+    
+    generate
+        for (i = 1; i < N; i++) begin : adder_gen
+            FullAdder FA (
+                .a(sum[i-1]), 
+                .b(shifted_partial[i]), 
+                .cin(0),
+                .s(sum[i]), 
+                .cout()
+            );
+        end
+    endgenerate
+    
+    assign p = sum[N-1];
+    
 endmodule
